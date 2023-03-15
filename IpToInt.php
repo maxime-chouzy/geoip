c22d494afdf9873657f017c43ec44c04ffc335b0
Maxime Chouzy <maxime.chouzypro@gmail.com>
2023-03-15 14:17:30 +0100

    t

--- IpToInt.php
+++ IpToInt.php
@@ -1,17 +1,22 @@
 <?php
 
 function IpToInt(){
-    // $ipRequest = "37.58.179.26";
     if( isset( $_GET['submit'])){
 
         $ipRequest = $_GET['ip_search'];
-        $ipSlice = explode(".", $ipRequest);
+        $longIp = ip2long($ipRequest);
 
-        // ip = ip3 + ip2 * 256 + ip1 * 256 * 256 + ip0 * 256 * 256 * 256
-        $ip = $ipSlice[3] + $ipSlice[2] * 256 + $ipSlice[1] * 256 * 256 + $ipSlice[0] * 256 * 256 * 256;
         echo " IP : ".$ipRequest;
         echo "<br>";
-        echo "Resultat IP TO INT : ".$ip;
+        echo "Resultat IP TO INT : ".$longIp;
+
+        if ($long == -1 || $long === FALSE) {
+            echo 'IP invalide, merci d\'essayer encore';
+        } else {
+            echo $ip   . "\n";            
+            echo $long . "\n";            
+            printf("%u\n", ip2long($ipRequest));
+}
     }
 }
 
